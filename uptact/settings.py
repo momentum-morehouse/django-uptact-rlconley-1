5b2ebacc841454ba2892d8404e19ed0401858fab
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2020-11-02 21:39:31 -0500

    add django registration redux

--- uptact/settings.py
+++ uptact/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'registration',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
